e004da5237f6ce1840b4993d4545e4a17be57c2e
Zach-Russell Russell <zacharyrussell01@gmail.com>
2024-04-27 06:45:04 +0000

    finish parts 1, 2, and 3

--- get-message.py
+++ get-message.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 # Set up your SQS queue URL and boto3 client
-url = "https://sqs.us-east-1.amazonaws.com/440848399208/xxxxxxx"
+url = "https://sqs.us-east-1.amazonaws.com/440848399208/zhr8wex"
 sqs = boto3.client('sqs')
 
 def delete_message(handle):
@@ -18,40 +18,72 @@ def delete_message(handle):
     except ClientError as e:
         print(e.response['Error']['Message'])
 
+
+
+#need to do x10 times, without running separatly 10 times
 def get_message():
-    try:
-        # Receive message from SQS queue. Each message has two MessageAttributes: order and word
-        # You want to extract these two attributes to reassemble the message
-        response = sqs.receive_message(
-            QueueUrl=url,
-            AttributeNames=[
-                'All'
-            ],
-            MaxNumberOfMessages=1,
-            MessageAttributeNames=[
-                'All'
-            ]
-        )
-        # Check if there is a message in the queue or not
-        if "Messages" in response:
-            # extract the two message attributes you want to use as variables
-            # extract the handle for deletion later
-            order = response['Messages'][0]['MessageAttributes']['order']['StringValue']
-            word = response['Messages'][0]['MessageAttributes']['word']['StringValue']
-            handle = response['Messages'][0]['ReceiptHandle']
 
-            # Print the message attributes - this is what you want to work with to reassemble the message
-            print(f"Order: {order}")
-            print(f"Word: {word}")
+    #fancy method of initalizing arrays, learnt it when I was grinding Leetcode for job interviews
+    myWords = [""] * 10
 
-        # If there is no message in the queue, print a message and exit    
-        else:
-            print("No message in the queue")
-            exit(1)
-            
-    # Handle any errors that may occur connecting to SQS
-    except ClientError as e:
-        print(e.response['Error']['Message'])
+
+    count = 0
+    while count < 10:
+        try:
+            # Receive message from SQS queue. Each message has two MessageAttributes: order and word
+            # You want to extract these two attributes to reassemble the message
+            response = sqs.receive_message(
+                QueueUrl=url,
+                AttributeNames=[
+                    'All'
+                ],
+                MaxNumberOfMessages=1,
+                MessageAttributeNames=[
+                    'All'
+                ]
+            )
+            # Check if there is a message in the queue or not
+            if "Messages" in response:
+                # extract the two message attributes you want to use as variables
+                # extract the handle for deletion later
+                order = response['Messages'][0]['MessageAttributes']['order']['StringValue']
+                word = response['Messages'][0]['MessageAttributes']['word']['StringValue']
+                handle = response['Messages'][0]['ReceiptHandle']
+
+                # Print the message attributes - this is what you want to work with to reassemble the message
+                print(f"Order: {order}")
+                print(f"Word: {word}")
+
+                #assign words in order, use int() to index array
+                myWords[int(order)] = word
+
+                count += 1
+
+            # If there is no message in the queue, print a message and exit    
+            #else:
+                #print("No message in the queue")
+
+                #exit(1)
+                
+        # Handle any errors that may occur connecting to SQS
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+
+            #break the while loop
+            break
+
+    #accumulate words from array into string
+    ret = ""
+    for n in range(len(myWords)):
+        #add word to return var
+        ret += myWords[n]
+
+        #if not last word, add space after
+        if n != len(myWords) - 1:
+            myWords += " "
+    
+    #display string
+    print(ret)
 
 # Trigger the function
 if __name__ == "__main__":
